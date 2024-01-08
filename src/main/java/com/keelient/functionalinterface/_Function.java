a4097c22edfa5326d40f2b5479ce6689d890a4db
Marco Scibetta <marco.scibetta@eng.it>
2024-01-08 16:38:41 +0100

    BiFunction

--- src/main/java/com/keelient/functionalinterface/_Function.java
+++ src/main/java/com/keelient/functionalinterface/_Function.java
@@ -1,9 +1,11 @@
 package com.keelient.functionalinterface;
 
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class _Function {
     public static void main(String[] args) {
+        //Function
         int increment = incrementByOne(0);
         System.out.println("\nincrement I: " + increment);
 
@@ -16,6 +18,11 @@ public class _Function {
         Function<Integer, Integer> addByOneAndThenMultiplyByTen = incrementByOneFunction.andThen(multiplyByTenFunction);
         int addByOneAndThenMultiplyByTenVar = addByOneAndThenMultiplyByTen.apply(1);
         System.out.println("\naddByOneAndThenMultiplyByTenVar: " + addByOneAndThenMultiplyByTenVar);
+
+        //BiFunction
+        int incrementByOneAndMultiplyBiFunctionVar = incrementByOneAndMultiplyBiFunction.apply(1, 10);
+        System.out.println("\nincrementByOneAndMultiplyBiFunctionVar: " + incrementByOneAndMultiplyBiFunctionVar);
+
     }
 
     static Function<Integer, Integer> incrementByOneFunction = number -> number + 1;
@@ -26,4 +33,12 @@ public class _Function {
     static int incrementByOne(int number) {
         return number + 1;
     }
+
+    static int incrementByOneAndMultiply(int number, int numToMultiplyBy) {
+        return (number + 1) * numToMultiplyBy;
+    }
+
+    static BiFunction<Integer, Integer, Integer> incrementByOneAndMultiplyBiFunction =
+            (numberToIncrementByOne, numberToMultiplyBy) -> (numberToIncrementByOne + 1) * numberToMultiplyBy;
+
 }
